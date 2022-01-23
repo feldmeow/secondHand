a99849473f1d69d469bc6c6c15b973825f530efe
Jauhen Feldman <feldmeow@gmail.com>
2022-01-24 01:06:35 +0300

    add burger-menu scripts

--- scripts/modules/burgerMenu.js
+++ scripts/modules/burgerMenu.js
@@ -1,9 +1,25 @@
-const burgerMenu = ({ classActive, selectorBtn, selectorMenu }) => {
+const burgerMenu = ({
+	classActive,
+	selectorBtn,
+	selectorMenu,
+	selectorClose,
+	parentSelector,
+}) => {
 	const btn = document.querySelector(selectorBtn);
 	const menu = document.querySelector(selectorMenu);
 
 	btn.addEventListener('click', () => {
 		menu.classList.toggle(classActive);
+	});
+	menu.addEventListener('click', (event) => {
+		if (event.target.closest(selectorClose)) {
+			menu.classList.remove(classActive);
+		}
+	});
+	document.addEventListener('click', (event) => {
+		if (!event.target.closest(parentSelector)) {
+			menu.classList.remove(classActive);
+		}
 	});
 };
 
