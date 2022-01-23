2e95e3f6bc7ae5745bd08e8f395e63bef3441ab9
Jauhen Feldman <feldmeow@gmail.com>
2022-01-24 01:08:50 +0300

    add burger, search, drop-down, slider scripts

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,29 @@
 import burgerMenu from './modules/burgerMenu.js';
+import searchControl from './modules/searchControl.js';
+import dropDown from './modules/dropDown.js';
+import slider from './modules/slider';
 
 burgerMenu({
 	selectorMenu: '.navigation',
 	selectorBtn: '.navigation__btn',
 	classActive: 'navigation_active',
+	selectorClose: '.navigation__link, .header__btn',
+	parentSelector: '.header',
 });
+
+searchControl({
+	activateBtn: '.search__button',
+	closeBtn: '.search__close',
+	selectorSearch: '.header__search',
+	classActivate: 'search_active',
+	searchInput: '.search__input',
+});
+
+dropDown({
+	dropTitle: '.footer__subtitle',
+	activeSelectorTo: '.footer__nav-item',
+	activeClass: 'footer__item-active',
+	breakpoint: 630,
+});
+
+document.addEventListener('click', (e) => e.preventDefault());
