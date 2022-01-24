e2f4beef9be2aba18625ee9c57eb910a40e93e94
Jauhen Feldman <feldmeow@gmail.com>
2022-01-24 18:38:46 +0300

    init & export slider

--- scripts/modules/slider.js
+++ scripts/modules/slider.js
@@ -1,3 +1,22 @@
-const slider = () => {};
+import Swiper from './swiper-bundle.esm.browser.min.js';
+const slider = ({
+	selectorSlider,
+	selectorPagination: el,
+	bulletClass,
+	bulletActiveClass,
+}) => {
+	new Swiper(selectorSlider, {
+		autoplay: true,
+		loop: true,
+		effect: 'coverflow',
+		pagination: {
+			el,
+			type: 'bullets',
+			bulletClass,
+			bulletActiveClass,
+			clickable: true,
+		},
+	});
+};
 
 export default slider;
