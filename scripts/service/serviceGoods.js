4cb196bd163487c95d1744a6113e1a40c802f0d2
Jauhen Feldman <feldmeow@gmail.com>
2022-02-01 23:37:07 +0300

    add request from server

---
+++ scripts/service/serviceGoods.js
@@ -0,0 +1,18 @@
+const URL_SERVER = 'http://localhost:3000';
+const URL_POSTFIX = '/api/goods';
+
+const getGoods = (query = '') =>
+	fetch(URL_SERVER + URL_POSTFIX + query)
+		.then((response) => {
+			if (response.ok) {
+				return response.json();
+			}
+			return new Error(response.statusText);
+		})
+		.catch((err) => console.error(err));
+
+const serviceGoods = async (fn, query) => {
+	fn(await getGoods(query));
+};
+
+export default serviceGoods;
