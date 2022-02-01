3f99a0753504d7b55da2b0993e0becc45573dcf5
Jauhen Feldman <feldmeow@gmail.com>
2022-02-01 23:37:40 +0300

    render on category click

---
+++ scripts/modules/interceptLink.js
@@ -0,0 +1,24 @@
+import renderGoods from './renderGoods.js';
+
+const handlerClick = (e) => {
+	const target = e.target.closest('a');
+
+	if (target) {
+		const href = target.getAttribute('href');
+		if (href[0] === '?') {
+			e.preventDefault();
+			history.pushState(
+				href.substring(1),
+				href.substring(1),
+				location.pathname + href
+			);
+			renderGoods(href);
+		}
+	}
+};
+
+const interceptLink = () => {
+	document.body.addEventListener('click', handlerClick);
+};
+
+export default interceptLink;
